54c9199c1ac616a29267662f970d17c984593424
RafaelaEllensburg <71078526+RafaelaEllensburg@users.noreply.github.com>
2022-07-11 16:07:17 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,27 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by{" "}
+        <a
+          href="https://www.rafaelaellensburg.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Rafaela Ellensburg
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/RafaelaEllensburg/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
