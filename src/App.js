8e136798052aa34b982adff57c48e8488c5d7409
RafaelaEllensburg <71078526+RafaelaEllensburg@users.noreply.github.com>
2022-07-11 16:57:37 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by{" "}
-        <a
-          href="https://www.rafaelaellensburg.com"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Rafaela Ellensburg
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/RafaelaEllensburg/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.rafaelaellensburg.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Rafaela Ellensburg
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/RafaelaEllensburg/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
